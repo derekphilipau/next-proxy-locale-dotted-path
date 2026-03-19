a7149b18c55b5787444bff741084ba59a5035a0d
Derek Philip Au <derek@derekau.net>
2026-03-19 08:01:26 -0400

    minimal repro of issue

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,3 @@
-import { ReproHome } from '@/components/repro-home'
-
 type LocaleParams = Promise<{ locale: string }>
 
 export default async function LocalePage({
@@ -9,5 +7,18 @@ export default async function LocalePage({
 }) {
   const { locale } = await params
 
-  return <ReproHome locale={locale} />
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: 24, lineHeight: 1.5 }}>
+      <h1>Locale {locale}</h1>
+      <p>
+        <a href="/en">Go to /en</a> | <a href="/es">Go to /es</a>
+      </p>
+      <p>
+        <a href="/nonexistent-file.png">Test /nonexistent-file.png</a> (Should 404, not 500)
+      </p>
+      <p>
+        <a href="/favicon.ico">Test /favicon.ico</a> (Should 200)
+      </p>
+    </main>
+  )
 }
